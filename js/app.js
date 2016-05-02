8827bc4df934a49a12e260579577c725daf6f5b8
Lauren C. Kennedy <laurenckennedy@gmail.com>
2016-05-02 14:45:32 -0400

    wdinstagram index/show with styling

--- js/app.js
+++ js/app.js
@@ -1,5 +1,42 @@
 "use strict";
 
 (function(){
-  angular.module("wdinstagram", [])
+  angular
+  .module("wdinstagram", [
+    "ui.router"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ])
+  .controller("wdinstagramIndexController", IndexControllerFunc)
+  .controller("wdinstagramShowController", ShowControllerFunc)
+
+  function RouterFunction($stateProvider) {
+    $stateProvider
+    .state("wdinstagramIndex", {
+      url: "/grams",
+      templateUrl: "js/wdinstagram/index.html",
+      controller: "wdinstagramIndexController",
+      controllerAs: "indexVm"
+    })
+    .state("wdinstagramShow", {
+      url:"grams/:id",
+      templateUrl: "js/wdinstagram/show.html",
+      controller: "wdinstagramShowController",
+      controllerAs: "showVm"
+    })
+  }
+
+function IndexControllerFunc(){
+  var indexVm = this;
+  indexVm.grams = grams;
+
+}
+
+ShowControllerFunc.$inject = ["$stateParams"];
+function ShowControllerFunc($stateParams) {
+  var showVm = this;
+  showVm.gram = grams[$stateParams.id]
+}
 })();
---
+++ js/grams.js
@@ -0,0 +1,8 @@
+var grams = [
+  {photo_url:"http://www.picklesnhoney.com/wp-content/uploads/2015/10/vegan-oreo-chocolate-cake-recipe-3.jpg", author:"LK", body:"Vegan Oreo Chocolate Cake"},
+  {photo_url:"http://jessexplainsitall.com/wp-content/uploads/2015/08/tips-to-cook-vegan-food-rainbow-fruit.jpg", author:"LK", body:"Fruity Design"},
+  {photo_url:"https://s-media-cache-ak0.pinimg.com/736x/fb/03/1d/fb031d36bfe3a73ea1db2c78ec23ea30.jpg", author:"Minions", body:"Tofu & Quinoa Salad"},
+  {photo_url:"https://putthatcheeseburgerdown.files.wordpress.com/2013/06/sauteedspinachcrispygarlic_145_600.jpg", author:"LK", body:"Sauteed spinach with garlic"},
+  {photo_url:"http://cdn.unconventionalbaker.com/wp-content/uploads/2014/07/Raw-Vegan-and-Gluten-Free-Black-and-White-Chocolate-Cake_Ice-Box-Cake.jpg", author:"LK", body:"Raw Vegan and Gluten-Free Black and White Chocolate Cake "},
+  {photo_url:"http://ohmyveggies.com/wp-content/uploads/2015/12/chickpea_meatloaf_connoisseurusveg-600x600.jpg", author:"LK", body:"Veggie Loaf"},
+  ];
